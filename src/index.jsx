ac45c44ea95c5b0369ed775b1d02874d6d45f69a
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-07-22 15:23:07 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@
 import ReactDOM from "react-dom/client";
 
 // import "./index.css";
+// eslint-disable-next-line no-unused-vars
 import styles from "./index.module.css";
 import App from "./App";
 
